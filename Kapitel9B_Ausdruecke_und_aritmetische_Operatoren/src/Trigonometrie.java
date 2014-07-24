94389359de5d37f1fa7c5383ecb0684b5dd505c3
Jan Seidler <janseidler@192.168.30.20>
2014-07-24 12:48:50 +0200

    Kapitel9B geaenderrt

--- Kapitel9B_Ausdruecke_und_aritmetische_Operatoren/src/Trigonometrie.java
+++ Kapitel9B_Ausdruecke_und_aritmetische_Operatoren/src/Trigonometrie.java
@@ -1,18 +1,18 @@
-
 public class Trigonometrie {
 
 	public static void main(String[] args) {
 
 		double zahl, sinx, cosx, ergebnis;
 		zahl = 0.5235;
-		sinx = Math.sin( zahl );
-		cosx = Math.cos( zahl );
-		ergebnis = (Math.pow(sinx, 2)) + (Math.pow(cosx,2));
 		
-		System.out.println("Sinus: " + sinx 
-				+ "\nCosinus: " + cosx + "\nErgebnis: " + ergebnis);
+		sinx = Math.sin(zahl);
+		cosx = Math.cos(zahl);
 		
-		
+		ergebnis = (Math.pow(sinx, 2)) + (Math.pow(cosx, 2));
+
+		System.out.println("Sinus: " + sinx + "\nCosinus: " + cosx
+				+ "\nErgebnis: " + ergebnis);
+
 	}
 
 }
