b771f0edaeace5cdd915d91a3b1cbde46babde5c
Jan Seidler <janseidler@192.168.30.20>
2014-07-24 13:25:06 +0200

    Kapitel 9C einbinden

--- Kapitel9C_Objektdaten/src/Watson.java
+++ Kapitel9C_Objektdaten/src/Watson.java
@@ -3,12 +3,29 @@ public class Watson {
 
 	public static void main(String[] args) {
 
-		String str;
-		str = new String("Das sind meine Methoden, mein lieber Watson!");
-		int laenge;
-		laenge = str.length();
-		System.out.println(str);
-		System.out.println("Die Laenge des Strings ist: " + laenge);
+	    String str;
+	    int    len;
+
+	    str = new String( "Elementary, my dear Watson!" );
+	    len = str.length();
+	    System.out.println("Die Laenge ist: " + len );  // -> 27
+	    
+	    str = new String( " Elementary, my dear Watson! " ); // f�hrendes + nachfolgendes Leerzeichen
+	    len = str.length();
+	    System.out.println("Die Laenge ist: " + len );  // -> 29
+	    
+	    str = new String( "		Elementary, my dear Watson!		" ); // 2 f�hrende + nachfolgende Tabzeichen
+	    len = str.length();		
+	    System.out.println("Die Laenge ist: " + len );  // -> 31
+	    
+	    str = new String( "Elementary,	my	dear	Watson!" ); // Im String, statt Leerzeichen Tabzeichen
+	    len = str.length();
+	    System.out.println("Die Laenge ist: " + len );  // -> 27
+
+	    str = new String( "Elementary, my dear Watson!!!" );
+	    len = str.length();
+	    System.out.println("Die Laenge ist: " + len );  // -> 29    
+	        
 	}
 
 }
